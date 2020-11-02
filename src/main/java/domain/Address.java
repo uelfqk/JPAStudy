755ee35a71aa85e74a747244559afacebeb6ded6
root <root@co.kr>
2020-11-03 00:28:55 +0900

    Jpa SampleCode ValueType

--- src/main/java/domain/Address.java
+++ src/main/java/domain/Address.java
@@ -1,6 +1,7 @@
 package domain;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -8,6 +9,12 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 
     public Address() {
 
@@ -17,23 +24,28 @@ public class Address {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
 
     public String getStreet() {
         return street;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
 
     public String getZipcode() {
         return zipcode;
     }
 
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) &&
+                Objects.equals(street, address.street) &&
+                Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
     }
 }
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,14 +1,13 @@
 package hellojpa;
 
-import domain.Address;
-import domain.Member;
-import domain.Period;
+import domain.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 public class JpaMain {
 
@@ -24,26 +23,96 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address = new Address();
-            address.setCity("111");
-            address.setStreet("111");
-            address.setZipcode("111");
+            TestClass test = new TestClass.TestClassBuilder("i9", "500GB")
+                    .setGraphicCardEnabled(true)
+                    .setBluetoothEnabled(true)
+                    .build();
 
-            Member member1 = new Member();
-            member1.setName("member1");
-            member1.setAddress(address);
-            em.persist(member1);
+            test.getCpu();
+            test.getHdd();
+            test.isGraphicCardEnabled();
+            test.isBluetoothEnabled();
 
-            Member member2 = new Member();
-            member2.setName("member2");
-            member2.setAddress(address);
-            em.persist(member2);
+            Member member = new Member();
 
-            member1.getAddress().setCity("zzz-");
+            member.setName("root");
+            member.setAddress(new Address("111", "222", "333"));
 
-            member1.getName();
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
 
-            member1.getId();
+            member.getAddressHistory().add(new AddressEntity("city", "street", "zipcode"));
+            member.getAddressHistory().add(new AddressEntity("city1", "street1", "zipcode1"));
+
+            // 라이프사이클이 같이 돌아간다.
+            // 값타입 컬렉션도 본인의 생명주기가 없다.
+            // 모든 컬렉션의 생명주기가 엔티티에 의존한다.
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for(String favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFoods = " + favoriteFood);
+            }
+            Address a = findMember.getAddress();
+            findMember.setAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            //치킨 => 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            findMember.getAddressHistory().remove(new AddressEntity("city", "street", "zipcode"));
+            findMember.getAddressHistory().add(new AddressEntity("city2", "street2", "zipcode2"));
+
+            //Address address = new Address();
+            //Address address1 = new Address();
+
+            // 자바는 참조값을 비교하기 때문에 == 결과가 false
+            //System.out.println("address1 == address " + (address1 == address));
+            // 기본 equals()는 == 비교이다.
+            // equals()를 재정의해서 사용해야한다. 주로 모든필드를 사용
+            //System.out.println("address1.equals(address) = " + address1.equals(address));
+            // 동일성 비교 == 인스턴스의 참조값 비교
+            // 동등성 비교 equals() 인스턴스의 값을 비교
+
+//            Address address = new Address("111", "222", "333");
+//
+//            Member member1 = new Member();
+//            member1.setName("member1");
+//            member1.setAddress(address);
+//            em.persist(member1);
+//
+//            Address address1 = new Address(address.getCity(), address.getStreet(), "zipzip");
+//
+//            Member member2 = new Member();
+//            member2.setName("member2");
+//            member2.setAddress(address1);
+//            em.persist(member2);
+
+            // 자바의 기본타입은 값을 복사함으로 복사한 변수를 변경해도 서로 영향을 받지 않음
+            //int a = 10;
+            //int b = a;
+            //b = 4;
+
+            // 자바의 객체타입은 값을 복사하는것이 아니라 참조를 공유한다.
+            // = 연산자를 막을 수 있는 방법이 없다.
+            //Address address2 = new Address("city", "street", "zipcode");
+            //Address address3 = address2;
+
+            // 따라서 불변 객체로 설정해야한다.
+            // String과 Integer가 대표적
+            // 생성장로만 설정하고 수정자를 막는것 setter X, setter를 private로 변경
+            // 값을 변경하고 싶은 경우 아래와 같이 새로 만들어서 변경해야한다.
+            // 아니면 다른 방법들도 있으니 찾아봐라.
+            // 불변으로 설정함으로서 다른 데이터를 변경하는 큰 재앙을 막을수 있다.
+            //Address newAddress = new Address(address2.getCity(), address2.getStreet(), "zipcode2");
+
+
 
 //            Movie movie = new Movie();
 //            movie.setName("바람");
